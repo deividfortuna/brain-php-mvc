c2725bfd04cd871f61b7ae3b4b2d21fa78987e6d
Deivid Fortuna <deividfortuna@gmail.com>
2015-09-30 08:48:25 -0300

    Update View.php

--- lib/brain/View.php
+++ lib/brain/View.php
@@ -2,64 +2,29 @@
 namespace Brain;
 
 /**
- * Controla a exibição de views e uso de suas variáveis.
  *
- * @author  Felipe Toreti <toreti@gmail.com>
+ * @author  Deivid Fortuna<deividfortuna@gmail.com>, Felipe Toreti <toreti@gmail.com>
  * @version 09/06/2014
  * @package Brain
  */
 class View
 {
-    /**
-     * @var mixed[] Variáveis a serem utilizadas na view.
-     */
     private $data = array();
-
-    /**
-     * @var string Diretório onde estão as views.
-     */
     public $dirView;
-
-    /**
-     * @var string Nome do layout utilizado.
-     */
     private $layout;
-
-    /**
-     * @var string Nome da view utilizada.
-     */
     private $view;
-
-    /**
-     * @var boolean do_cache sinaliza se a pagina deve ser cacheada
-     */
     private $do_cache = false;
-
-    /**
-     * Retorna o array das variáveis utilizadas na view.
-     *
-     * @return string
-     */
+    
     public function getData()
     {
         return $this->data;
     }
-
-    /**
-     * Retorna o layout a ser exibido.
-     *
-     * @return string
-     */
+    
     public function getLayout()
     {
         return $this->layout;
     }
-
-    /**
-     * Retorna a view a ser exibida.
-     *
-     * @return string
-     */
+    
     public function getView()
     {
         return $this->view;
@@ -85,24 +50,12 @@ class View
         return $this;
     }
 
-    /**
-     * Define o layout a ser exibido.
-     *
-     * @param $layout
-     * @return View
-     */
     public function setLayout($layout)
     {
         $this->layout = $layout;
         return $this;
     }
 
-    /**
-     * Define a view a ser exibida.
-     *
-     * @param string $view
-     * @return View
-     */
     public function setView($view)
     {
         $this->view = $view;
@@ -112,13 +65,6 @@ class View
     public function setDoCache(){
         $this->do_cache = true;
     }
-
-    /**
-     * Exibe o arquivo informado e extrai suas variáveis.
-     *
-     * @param string $view
-     * @param array $data
-     */
 
     protected function show($view, array $data = null)
     {
@@ -130,42 +76,21 @@ class View
     }
 
 
-    /**
-     * Exibe o arquivo informado e utiliza as variáveis definidas no controller.
-     *
-     * @param string $view
-     */
     protected function showWithData($view)
     {
         $this->show($view, $this->data);
     }
-
-    /**
-     * Atalho para exibir arquivos dentro da pasta "element".
-     * Não tem acesso as variáveis definidas no controller, apenas às informadas por parâmetro.
-     *
-     * @param string $element
-     * @param array $data
-     */
+    
     protected function showElement($element, array $data = null)
     {
         $this->show('element/'.$element, $data);
     }
-
-    /**
-     * Atalho para exibir arquivos dentro da pasta "element".
-     * Utiliza as variáveis definidas no controller.
-     *
-     * @param string $element
-     */
+    
     protected function showElementWithData($element)
     {
         $this->show('element/'.$element, $this->data);
     }
 
-    /**
-     * Exibe o layout que contém a view principal.
-     */
     public function showLayout()
     {
         if($this->do_cache) {
@@ -182,13 +107,7 @@ class View
         }
         exit;
     }
-
-    /**
-     * Exibe a view principal.
-     *
-     * Caso o nome da view não tenha sido declarado,
-     * utiliza o nome do controller como pasta e o da action como arquivo.
-     */
+    
     protected function showView()
     {
         if (is_null($this->view)) {
